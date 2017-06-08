eb015c250e384c66e38314289f14e5de9550aa6a
Diamond <Diamond@DESKTOP-GRV4U7V>
2017-06-08 09:30:33 +0800

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_HomeWork2_BookStore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
