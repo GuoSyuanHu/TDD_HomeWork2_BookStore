86726978f25e967b2eadb068c92b141c51f5e57c
Diamond <Diamond@DESKTOP-GRV4U7V>
2017-06-08 10:00:32 +0800

    第一個情境 : 第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ Book.cs
@@ -0,0 +1,9 @@
+﻿namespace TDD_HomeWork2_BookStore
+{
+    public class Book
+    {
+        public int episode { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+    }
+}
--- ShoppingDiscount.cs
+++ ShoppingDiscount.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TDD_HomeWork2_BookStore
 {
@@ -7,8 +9,61 @@ namespace TDD_HomeWork2_BookStore
     public class ShoppingDiscount
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Cart_Add_Potter1_has_1_Should_Be_100()
         {
+            // arrange
+            List<Book> bookList = new List<Book>()
+            {
+                new Book {episode = 1, quantity = 1, price = 100 },
+            };
+
+            // act
+            decimal actual = this.CalculatePrice(bookList);
+            // assert
+            var excepted = 100m;
+
+            Assert.AreEqual(excepted, actual);
+        }
+
+        private decimal CalculatePrice(List<Book> bookList)
+        {
+            var bookTypeCount = bookList.Count();
+            decimal totalPrice = default(decimal);
+
+            while (bookList.Any(b => b.quantity > 0))
+            {
+                var tempPrice = default(decimal);
+                foreach (var book in bookList)
+                {
+                    if (book.quantity == 0)
+                    {
+                        continue;
+                    }
+                    tempPrice += book.price;
+                    book.quantity--;
+                }
+                totalPrice += tempPrice * GetDiscountSetting(bookTypeCount);
+            }
+            return totalPrice;
+        }
+
+        private decimal GetDiscountSetting(int bookTypeCount)
+        {
+            switch (bookTypeCount)
+            {
+                case 1:
+                    return 1m;
+                case 2:
+                    return 0.95m;
+                case 3:
+                    return 0.9m;
+                case 4:
+                    return 0.8m;
+                case 5:
+                    return 0.75m;
+                default:
+                    return 0m;
+            }
         }
     }
 }
