196059787193e35d104adc1d5e3a1c6e90b729b4
Diamond <Diamond@DESKTOP-GRV4U7V>
2017-06-08 10:07:37 +0800

    (紅燈)第六個情境 : 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- ShoppingDiscount.cs
+++ ShoppingDiscount.cs
@@ -99,6 +99,24 @@ namespace TDD_HomeWork2_BookStore
             Assert.AreEqual(excepted, actual);
         }
 
+        [TestMethod]
+        public void Cart_Add_Potter1_has_1_Potter2_has_1_Potter3_has_2_Should_Be_370()
+        {
+            // arrange
+            List<Book> bookList = new List<Book>
+            {
+                new Book { episode = 1, quantity=1, price = 100},
+                new Book { episode = 2, quantity=1, price = 100},
+                new Book { episode = 3, quantity=2, price = 100},
+            };
+            // act
+            decimal actual = this.CalculatePrice(bookList);
+            // assert
+            var excepted = 370m;
+
+            Assert.AreEqual(excepted, actual);
+        }
+
         private decimal CalculatePrice(List<Book> bookList)
         {
             var bookTypeCount = bookList.Count();
